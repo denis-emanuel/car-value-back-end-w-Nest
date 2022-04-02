dda4b909f4f556de040db6056d8a95b4351eab8b
Denis Emanuel Pop <pop.denis1902@gmail.com>
2022-04-02 16:43:08 +0300

    heroku deployment

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { env } from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
